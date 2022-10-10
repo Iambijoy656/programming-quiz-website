965b94eee9e6eaabf8d109537a34954da403922d
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-10-11 03:19:30 +0600

    dynamic question set

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Quiz = () => {
+const Quiz = ({ questionObj }) => {
+    console.log(questionObj)
+    const { question, options } = questionObj
+
     return (
         <div>
             <div className="bg-white text-gray-100 rounded-lg shadow-lg my-4">
                 <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-md">
                     <div className="flex items-center justify-between">
                         <span className="text-sm text-gray-400"> 1 </span>
-                        <Link rel="noopener noreferrer" href="#" className="px-2 py-1 font-bold rounded bg-violet-400 text-gray-900">Javascript</Link>
+                        <Link rel="noopener noreferrer" href="#" className="px-2 py-1 font-bold rounded bg-violet-400 text-gray-900">Eye icon</Link>
                     </div>
                     <div className="mt-3">
-                        <Link rel="noopener noreferrer" href="#" className="text-2xl font-bold hover:underline text-blue-500">Nos creasse pendere crescit angelos etc</Link>
+                        <Link rel="noopener noreferrer" href="#" className="text-2xl font-bold hover:underline text-blue-500">{question}</Link>
                         <p className="mt-2 text-blue-500">Tamquam ita veritas res equidem. Ea in ad expertus paulatim poterunt. Imo volo aspi novi tur. Ferre hic neque vulgo hae athei spero. Tantumdem naturales excaecant notaverim etc cau perfacile occurrere. Loco visa to du huic at in dixi aër.</p>
                     </div>
                     <div className="flex items-center justify-between mt-4">
--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -4,16 +4,21 @@ import Quiz from './Quiz';
 
 const QuizPage = () => {
     const quizData = useLoaderData().data;
-    console.log(quizData)
+    // console.log(quizData)
 
-    const { name, questions
-    } = quizData
+    const { name, questions, id } = quizData
 
     return (
         <div>
             <h3 className='text-2xl text-center font-semibold my-4 text-blue-800'>Quiz of {name}</h3>
             {
-                questions.map(question => <Quiz></Quiz>)
+                questions.map(questionObj => <Quiz
+                    key={questionObj.id}
+                    questionObj={questionObj}
+
+                >
+
+                </Quiz>)
             }
 
         </div>
