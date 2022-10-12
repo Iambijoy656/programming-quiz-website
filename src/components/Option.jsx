1e4f4421397135b0be8ac46218eed1db920054ee
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-10-12 20:10:02 +0600

    option hover done

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -7,7 +7,7 @@ const Option = ({ option, questionObj, handleAnswer }) => {
 
     return (
         <button onClick={() => handleAnswer(questionObj, option)}>
-            <div className='border border-blue-400 bg-sky-200 p-5 my-1 mt-4 rounded-lg text-zinc-700 hover:bg-blue-100'>
+            <div className='border border-blue-400 bg-sky-200 p-5 my-1 mt-4 rounded-lg text-zinc-700 hover:bg-blue-300'>
                 {option}
 
             </div>
